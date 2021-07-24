124085c84144a6c06ba75f04235b513c2d76db7c
Ridham Sherathiya <35310564+ridham97@users.noreply.github.com>
2021-07-24 21:21:36 +0530

    Added files

---
+++ App.js
@@ -0,0 +1,125 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useRef } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  Dimensions,
+  Animated,
+} from "react-native";
+const { width, height } = Dimensions.get("screen");
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.47;
+
+const images = [
+  "https://images.unsplash.com/photo-1551316679-9c6ae9dec224?w=800&q=80",
+  "https://images.unsplash.com/photo-1562569633-622303bafef5?w=800&q=80",
+  "https://images.unsplash.com/photo-1503656142023-618e7d1f435a?w=800&q=80",
+  "https://images.unsplash.com/photo-1555096462-c1c5eb4e4d64?w=800&q=80",
+  "https://images.unsplash.com/photo-1517957754642-2870518e16f8?w=800&q=80",
+  "https://images.unsplash.com/photo-1546484959-f9a381d1330d?w=800&q=80",
+  "https://images.unsplash.com/photo-1548761208-b7896a6ff225?w=800&q=80",
+  "https://images.unsplash.com/photo-1511208687438-2c5a5abb810c?w=800&q=80",
+  "https://images.unsplash.com/photo-1548614606-52b4451f994b?w=800&q=80",
+  "https://images.unsplash.com/photo-1548600916-dc8492f8e845?w=800&q=80",
+];
+
+const data = images.map((image, index) => ({
+  key: String(index),
+  photo: image,
+  avatar_url: `https://randomuser.me/api/portraits/women/${Math.floor(
+    Math.random() * 40
+  )}.jpg`,
+}));
+
+export default function App() {
+  const scrollX = useRef(new Animated.Value(0)).current;
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Animated.FlatList
+        data={data}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.key}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width,
+          ];
+          const translateX = scrollX.interpolate({
+            inputRange,
+            outputRange: [-width * 0.7, 0, width * 0.7],
+          });
+          return (
+            <View
+              style={{
+                width,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  position: "relative",
+                }}
+              >
+                <View
+                  style={{
+                    width: ITEM_WIDTH,
+                    height: ITEM_HEIGHT,
+                    overflow: "hidden",
+                    alignItems: "center",
+                    borderRadius: 16,
+                  }}
+                >
+                  <Animated.Image
+                    source={{ uri: item.photo }}
+                    style={{
+                      width: ITEM_WIDTH * 1.4,
+                      height: ITEM_HEIGHT,
+                      resizeMode: "cover",
+                      transform: [
+                        {
+                          translateX: translateX,
+                        },
+                      ],
+                    }}
+                  />
+                </View>
+                <Image
+                  source={{ uri: item.avatar_url }}
+                  style={{
+                    width: 60,
+                    height: 60,
+                    borderRadius: 60,
+                    position: "absolute",
+                    bottom: -20,
+                    right: -20,
+                    borderColor: "white",
+                    borderWidth: 5,
+                  }}
+                />
+              </View>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
